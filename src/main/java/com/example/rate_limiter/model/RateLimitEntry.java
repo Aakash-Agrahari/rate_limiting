92f8331239ab307b36e0f4ab03f46554a4ef5331
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-09 14:47:06 +0530

    model entry for data collection related to reqests

--- src/main/java/com/example/rate_limiter/model/RateLimitEntry.java
+++ src/main/java/com/example/rate_limiter/model/RateLimitEntry.java
@@ -1,4 +1,14 @@
 package com.example.rate_limiter.model;
 
 public class RateLimitEntry {
+
+    private int requestCount;
+    private long windowStart;
+
+    public RateLimitEntry(long windowStart){
+        this.windowStart = windowStart;
+        this.requestCount = 0;
+    }
+
+    
 }
