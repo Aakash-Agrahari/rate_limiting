27e5c79a840d3c92a70658575a9af3b6ebfc75e3
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-09 21:55:14 +0530

    added description about fixed window RL algo

--- src/main/java/com/example/rate_limiter/limiter/FixedWindowRateLimiter.java
+++ src/main/java/com/example/rate_limiter/limiter/FixedWindowRateLimiter.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+
+//Fixed Window Rate Limiting divides time into fixed intervals, such as 1 minute, and allows a maximum number
+// of requests within each interval. For example, if the limit is 5 requests per minute, the system counts
+// requests from 10:00:00 to 10:00:59; once the user reaches 5 requests, further requests are rejected until
+// the next window starts at 10:01:00, when the counter resets to 0.
+
 @Component
 public class FixedWindowRateLimiter implements RateLimiter{
     private final ConcurrentHashMap<String, RateLimitEntry> clients =
