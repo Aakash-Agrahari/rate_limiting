933ed9d7130429a2f0dd8a91b78e53f932d1dfec
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-09 14:53:44 +0530

    fixed window rate limiter

--- src/main/java/com/example/rate_limiter/limiter/FixedWindowRateLimiter.java
+++ src/main/java/com/example/rate_limiter/limiter/FixedWindowRateLimiter.java
@@ -1,4 +1,23 @@
 package com.example.rate_limiter.limiter;
 
-public class FixedWindowRateLimiter {
+import com.example.rate_limiter.model.RateLimitEntry;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FixedWindowRateLimiter implements RateLimiter{
+    private final ConcurrentHashMap<String, RateLimitEntry> clients =
+            new ConcurrentHashMap<>();
+
+    private final int limit;
+    private final long windowSizeMillis;
+
+    public FixedWindowRateLimiter(int limit, long windowSizeMillis) {
+        this.limit = limit;
+        this.windowSizeMillis = windowSizeMillis;
+    }
+
+    @Override
+    public boolean allowRequest(String clientId) {
+        return false;
+    }
 }
