33a1f20ac081ee7c706f94ebef930e8649279071
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-10 21:46:18 +0530

    implemented the interface in the tokenbucketRL

--- src/main/java/com/example/rate_limiter/limiter/TokenBucketRateLimiter.java
+++ src/main/java/com/example/rate_limiter/limiter/TokenBucketRateLimiter.java
@@ -1,4 +1,17 @@
 package com.example.rate_limiter.limiter;
 
-public class TokenBucketRateLimiter {
+import com.example.rate_limiter.model.TokenBucket;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TokenBucketRateLimiter implements RateLimiter {
+    private static final int CAPACITY = 5;
+    private static final double REFILL_RATE = 1.0 / 2.0;
+
+    private final ConcurrentHashMap<String, TokenBucket> clients = new ConcurrentHashMap<>();
+
+    @Override
+    public boolean allowRequest(String clientId) {
+        return false;
+    }
 }
