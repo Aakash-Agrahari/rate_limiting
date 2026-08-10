b9259fbd9d5855b6b8002603cb1bf7f2b0f670f4
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-10 22:05:50 +0530

    added the bean annotation in the token bucket class

--- src/main/java/com/example/rate_limiter/limiter/TokenBucketRateLimiter.java
+++ src/main/java/com/example/rate_limiter/limiter/TokenBucketRateLimiter.java
@@ -1,9 +1,12 @@
 package com.example.rate_limiter.limiter;
 
 import com.example.rate_limiter.model.TokenBucket;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+
+@Component
 public class TokenBucketRateLimiter implements RateLimiter {
     private static final int CAPACITY = 5;
     private static final double REFILL_RATE = 1.0 / 2.0;
