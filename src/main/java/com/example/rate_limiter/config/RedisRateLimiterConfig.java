5ec1609069c9114a3b055853a2a947ed3da89418
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-12 18:07:59 +0530

    redis rate limiter and some configuration issue solved

--- src/main/java/com/example/rate_limiter/config/RedisRateLimiterConfig.java
+++ src/main/java/com/example/rate_limiter/config/RedisRateLimiterConfig.java
@@ -4,10 +4,46 @@ package com.example.rate_limiter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisRateLimiterConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(
+            RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, String> template =
+                new RedisTemplate<>();
+
+        template.setConnectionFactory(
+                connectionFactory
+        );
+
+        template.setKeySerializer(
+                new StringRedisSerializer()
+        );
+
+        template.setValueSerializer(
+                new StringRedisSerializer()
+        );
+
+        template.setHashKeySerializer(
+                new StringRedisSerializer()
+        );
+
+        template.setHashValueSerializer(
+                new StringRedisSerializer()
+        );
+
+        template.afterPropertiesSet();
+
+        return template;
+    }
+
 
     @Bean
     public DefaultRedisScript<Long> tokenBucketScript(){
--- src/main/java/com/example/rate_limiter/limiter/RedisTokenBucketRateLimiter.java
+++ src/main/java/com/example/rate_limiter/limiter/RedisTokenBucketRateLimiter.java
@@ -1,5 +1,41 @@
 package com.example.rate_limiter.limiter;
 
-public class RedisTokenBucketRateLimiter {
-    
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class RedisTokenBucketRateLimiter implements RateLimiter {
+    private final RedisTemplate<String, String> redisTemplate;
+    private final DefaultRedisScript<Long> tokenBucketScript;
+    private final int capacity;
+    private final double refillRate;
+
+    public RedisTokenBucketRateLimiter(
+            RedisTemplate<String, String> redisTemplate,
+            DefaultRedisScript<Long> tokenBucketScript,
+            @Value("${rate-limit.capacity}") int capacity,
+            @Value("${rate-limit.refill-rate}") double refillRate
+    ){
+        this.redisTemplate = redisTemplate;
+        this.tokenBucketScript = tokenBucketScript;
+        this.capacity = capacity;
+        this.refillRate = refillRate;
+    }
+
+    @Override
+    public boolean allowRequest(String clientId) {
+        String key = "rate-limit:" + clientId;
+        long currentTime = System.currentTimeMillis();
+        Long result = redisTemplate.execute(
+                tokenBucketScript,
+                Collections.singletonList(key),
+                String.valueOf(refillRate),
+                String.valueOf(currentTime)
+        );
+        return result != null && result == 1;
+    }
 }
