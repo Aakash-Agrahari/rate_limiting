750f1889684dc50b4de04fe3ac9441f0da919638
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-13 21:27:35 +0530

    modified the unnecessary config

--- src/main/java/com/example/rate_limiter/config/RedisRateLimiterConfig.java
+++ src/main/java/com/example/rate_limiter/config/RedisRateLimiterConfig.java
@@ -1,59 +1,25 @@
 package com.example.rate_limiter.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisRateLimiterConfig {
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(
-            RedisConnectionFactory connectionFactory) {
+    public DefaultRedisScript<Long> tokenBucketScript() {
 
-        RedisTemplate<String, String> template =
-                new RedisTemplate<>();
-
-        template.setConnectionFactory(
-                connectionFactory
-        );
-
-        template.setKeySerializer(
-                new StringRedisSerializer()
-        );
-
-        template.setValueSerializer(
-                new StringRedisSerializer()
-        );
-
-        template.setHashKeySerializer(
-                new StringRedisSerializer()
-        );
-
-        template.setHashValueSerializer(
-                new StringRedisSerializer()
-        );
-
-        template.afterPropertiesSet();
-
-        return template;
-    }
-
-
-    @Bean
-    public DefaultRedisScript<Long> tokenBucketScript(){
-        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        DefaultRedisScript<Long> script =
+                new DefaultRedisScript<>();
 
         script.setLocation(
                 new ClassPathResource(
                         "scripts/token_bucket.lua"
                 )
         );
+
         script.setResultType(Long.class);
 
         return script;
