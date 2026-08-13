4e17897e30954dc105f6999b7193b5cf71d7eed3
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-13 21:25:44 +0530

    few improvements in the lua script

--- src/main/resources/scripts/token_bucket.lua
+++ src/main/resources/scripts/token_bucket.lua
@@ -4,23 +4,34 @@ local capacity = tonumber(ARGV[1])
 local refillRate = tonumber(ARGV[2])
 local currentTime = tonumber(ARGV[3])
 
-local tokens = tonumber(redis.call('HGET', key, 'tokens'))
-local lastRefillTime = tonumber(redis.call('HGET', key, 'lastRefillTime'))
+local tokens = tonumber(
+        redis.call('HGET', key, 'tokens')
+)
+
+local lastRefillTime = tonumber(
+        redis.call('HGET', key, 'lastRefillTime')
+)
 
 if tokens == nil then
     tokens = capacity
     lastRefillTime = currentTime
 end
 
-local elapsedTime = (currentTime - lastRefillTime) / 1000.0
+local elapsedTime =
+(currentTime - lastRefillTime) / 1000.0
 
-local tokensToAdd = elapsedTime * refillRate
+local tokensToAdd =
+elapsedTime * refillRate
 
-tokens = math.min(capacity, tokens + tokensToAdd)
+tokens = math.min(
+        capacity,
+        tokens + tokensToAdd
+)
 
 lastRefillTime = currentTime
 
 if tokens < 1 then
+
     redis.call(
             'HSET',
             key,
@@ -29,6 +40,7 @@ if tokens < 1 then
             'lastRefillTime',
             lastRefillTime
     )
+
     return 0
 end
 
