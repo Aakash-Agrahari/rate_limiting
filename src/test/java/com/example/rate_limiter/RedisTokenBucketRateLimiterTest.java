9c08307efa872d8a64c7209e81e9e3d68fce1e86
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-08-14 22:20:16 +0530

    added requests related tests

--- src/test/java/com/example/rate_limiter/RedisTokenBucketRateLimiterTest.java
+++ src/test/java/com/example/rate_limiter/RedisTokenBucketRateLimiterTest.java
@@ -1,4 +1,54 @@
 package com.example.rate_limiter;
 
+
+import com.example.rate_limiter.limiter.RateLimiter;
+import com.example.rate_limiter.model.RateLimitResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 public class RedisTokenBucketRateLimiterTest {
+
+    @Autowired
+    private RateLimiter rateLimiter;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @BeforeEach
+    void cleanRedis(){
+        redisTemplate.delete("rate-limit: Aakash");
+        redisTemplate.delete("rate-limit: Sky");
+    }
+
+    @Test
+    void shouldAllowFiveInitialRequests(){
+        for(int i=1; i<=5; i++){
+            RateLimitResult result = rateLimiter.allowRequest("Aakash");
+            assertTrue(
+                    result.allowed(),
+                    "Request " + i + "should be allowed"
+            );
+        }
+    }
+
+    @Test
+    void shouldRejectSixthRequest(){
+        for(int i=0; i<5; i++){
+            rateLimiter.allowRequest("Aakash");
+        }
+        RateLimitResult result = rateLimiter.allowRequest("Aakash");
+
+        assertFalse(result.allowed());
+        assertEquals(5, result.limit());
+        assertEquals(0, result.remaining());
+        assertTrue(
+                result.retryAfterSeconds() > 0
+        );
+    }
 }
